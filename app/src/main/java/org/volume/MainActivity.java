e941c4594bece3a677a09d327247f01e6f411a22
Maxim Tkachenko <maxim@2dehands.be>
2016-04-08 17:04:18 +0200

    App beeps when volume changes.

--- app/src/main/java/org/volume/MainActivity.java
+++ app/src/main/java/org/volume/MainActivity.java
@@ -1,16 +1,13 @@
 package org.volume;
 
-import android.animation.Animator;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.location.LocationManager;
 import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.DecelerateInterpolator;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -20,13 +17,17 @@ import java.util.TimeZone;
 
 import static android.media.AudioManager.ADJUST_LOWER;
 import static android.media.AudioManager.ADJUST_RAISE;
+import static android.media.AudioManager.STREAM_MUSIC;
 
 public class MainActivity extends AppCompatActivity implements VolumeManager.OnVolumeChangeListener, SpeedManager.OnSpeedUpdateListener {
     private TextView speedView, timeView, volLevelView;
-    private View volUpView, volDownView, volChangeIndicatorView;
+    private View volUpView, volDownView;
+    private CheckBox beepView;
 
     private VolumeManager volumeManager;
     private SpeedManager speedManager;
+
+    private ToneGenerator beeper;
 
     private SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
 
@@ -55,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements VolumeManager.OnV
         volUpView = findViewById(R.id.vol_up);
         volDownView = findViewById(R.id.vol_down);
         volLevelView = (TextView) findViewById(R.id.vol_level);
-        volChangeIndicatorView = findViewById(R.id.vol_change_indicator);
+        beepView = (CheckBox) findViewById(R.id.beep_ckeckbox);
 
         dateFormat.setTimeZone(TimeZone.getTimeZone("GMT+1"));
 
@@ -67,6 +68,8 @@ public class MainActivity extends AppCompatActivity implements VolumeManager.OnV
 
         volumeManager.setOnVolumeChangeListener(this);
         speedManager.setOnSpeedUpdateListener(this);
+
+        beeper = new ToneGenerator(STREAM_MUSIC, 100);
 
         volUpView.setOnClickListener(volumeClickListener);
         volDownView.setOnClickListener(volumeClickListener);
@@ -91,43 +94,11 @@ public class MainActivity extends AppCompatActivity implements VolumeManager.OnV
     public void onVolumeChange(int oldLevel, int newLevel, int maxLevel) {
         volLevelView.setText(String.valueOf(newLevel));
 
-        int indicatorColor = oldLevel > newLevel ? R.color.vol_down_indicator : R.color.vol_up_indicator;
-        volChangeIndicatorView.setBackgroundColor(getResources().getColor(indicatorColor));
-
-        ObjectAnimator show = ObjectAnimator.ofFloat(volChangeIndicatorView, "alpha", 0, 1);
-        show.setInterpolator(new AccelerateInterpolator());
-        show.setDuration(150);
-
-        ObjectAnimator hide = ObjectAnimator.ofFloat(volChangeIndicatorView, "alpha", 1, 0);
-        hide.setInterpolator(new DecelerateInterpolator());
-        hide.setDuration(350);
-
-        volChangeIndicatorView.setVisibility(View.VISIBLE);
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playSequentially(show, hide);
-        animatorSet.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                volChangeIndicatorView.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
-
-        animatorSet.start();
+        if (beepView.isChecked()) {
+            boolean volumeIncreased = newLevel > oldLevel;
+            int beep = volumeIncreased ? ToneGenerator.TONE_DTMF_B : ToneGenerator.TONE_DTMF_1;
+            beeper.startTone(beep, 150);
+        }
     }
 
     @Override
